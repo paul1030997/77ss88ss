33775cd78b1077a2ff50262dc03df1be71c50c71
paul1030997 <paul1030997@gmail.com>
2025-02-15 18:29:30 +0800

    add

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from linebot.models import *
 import os
 import requests, json
 
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
-userID = os.environ['BYLEE']
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -23,28 +24,30 @@ def callback():
     return 'OK'
 
 
-@app.route("/api/test" , methods=['POST'])
-def testapi():
-  try:
-    msg = request.data   # 取得網址的 msg 參數
-    if msg != None:
-      msg_data = msg.decode("utf-8")
-      # 如果有 msg 參數，觸發 LINE Message API 的 push_message 方法
-      line_bot_api.push_message(userID, TextSendMessage(text=msg_data))
-      return msg
-    else:
-      return 'OK'
-  except:
-    print('error')
+# @app.route("/api/test" , methods=['POST'])
+# def testapi():
+#   try:
+#     msg = request.data   # 取得網址的 msg 參數
+    
+#     if msg != None:
+#       msg_data = msg.decode("utf-8")
+#       print(msg_data)
+#       # 如果有 msg 參數，觸發 LINE Message API 的 push_message 方法
+#       line_bot_api.push_message("Ua4c9e00a3662dddc35ee1d36665a8e27", TextSendMessage(text=msg_data))
+#       return msg
+#     else:
+#       return 'OK'
+#   except:
+#     print('error')
 
 
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.push_message("Ua4c9e00a3662dddc35ee1d36665a8e27", TextSendMessage(text='Hello World!!!'))
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     line_bot_api.push_message("Ua4c9e00a3662dddc35ee1d36665a8e27", TextSendMessage(text='Hello World!!!'))
+#     message = TextSendMessage(text=event.message.text)
+#     line_bot_api.reply_message(event.reply_token, message)
     
 import os
 if __name__ == "__main__":
